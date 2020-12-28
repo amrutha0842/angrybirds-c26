c55147c564d980efe538cfa3f0ca4e8bf6dd60a5
amrutha0842 <74587916+amrutha0842@users.noreply.github.com>
2020-12-28 13:11:48 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
@@ -18,12 +18,12 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
-    console.log("hi")
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+    log6 = new Log(230,180,80,PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -36,7 +36,14 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    var options = {
+        bodyA: bird.body,
+        bodyB: log6.body,
+        stifness:0.04,
+        length:10,
+    }
+    var chain = Constraint.create(options)
+    World.add(world,chain)
 }
 
 function draw(){
@@ -59,7 +66,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    log6.display();
     bird.display();
     platform.display();
 }
